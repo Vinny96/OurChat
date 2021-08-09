f92b38f4a7caa6bb81c22ead3e1badc258067655
Vinojen Gengatharan <gengatharanvinojen@gmail.com>
2021-08-09 17:36:31 -0400

    Initial Commit

---
+++ OurChat/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  OurChat
+//
+//  Created by Vinojen Gengatharan on 2021-08-09.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
