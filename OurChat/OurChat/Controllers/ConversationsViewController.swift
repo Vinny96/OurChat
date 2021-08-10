f828ee5da99e8ef3d25c52d642eaa79fc6fc6b7c
Vinojen Gengatharan <gengatharanvinojen@gmail.com>
2021-08-10 15:09:45 -0400

    Created functionality that will check if the user is logged in and take the user to the LogIn View controllerif they are not logged in

--- OurChat/OurChat/Controllers/ConversationsViewController.swift
+++ OurChat/OurChat/Controllers/ConversationsViewController.swift
@@ -9,9 +9,37 @@ import UIKit
 
 class ConversationsViewController: UIViewController {
 
+    var standardUserDefaults = UserDefaults.standard
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Converations"
+        view.backgroundColor = .red
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        checkIfUserIsLoggedIn()
+    }
+
+    
+    
+    
+    
+    
+    // MARK: - Functions
+    private func checkIfUserIsLoggedIn()
+    {
+        let valueReturned = standardUserDefaults.bool(forKey: UserDefaultKeys.isUserLoggedInKey)
+        if !valueReturned
+        {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false)
+        }
     }
 
 
