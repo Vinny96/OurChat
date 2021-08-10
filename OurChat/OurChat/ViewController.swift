614a4cd09d7f980443379e7eb4d306a903618a34
Vinojen Gengatharan <gengatharanvinojen@gmail.com>
2021-08-09 20:41:32 -0400

    Getting Started

--- OurChat/OurChat/ViewController.swift
+++ OurChat/OurChat/ViewController.swift
@@ -5,20 +5,13 @@
 //  Created by Vinojen Gengatharan on 2021-08-09.
 //
 
-import Cocoa
+import UIKit
 
-class ViewController: NSViewController {
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
     }
 
 
