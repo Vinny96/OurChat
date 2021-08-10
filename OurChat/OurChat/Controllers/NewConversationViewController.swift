5adc4fa7c4a5c9c58822968f8fadafafa4c690dd
Vinojen Gengatharan <gengatharanvinojen@gmail.com>
2021-08-10 15:07:30 -0400

    Created functionality that will check if the user is logged in or not and will take the user to the LoginViewController if they are not logged in

--- OurChat/OurChat/Controllers/NewConversationViewController.swift
+++ OurChat/OurChat/Controllers/NewConversationViewController.swift
@@ -9,21 +9,25 @@ import UIKit
 
 class NewConversationViewController: UIViewController {
 
+    // properties
+    var standardUserDefaults = UserDefaults.standard
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
     }
-    */
 
+    
+    
+    
+    
+    
+    // MARK: - Functions
+    
 }
