fd57732351cb6b75861b17e2e2a8b01ba4497943
Vinojen Gengatharan <gengatharanvinojen@gmail.com>
2021-08-11 12:55:11 -0400

    Add firebase code to AppDelegate.swift and add constants to constants.swift that will be used throughout the app

---
+++ OurChat/OurChat/Resources/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  OurChat
+//
+//  Created by Vinojen Gengatharan on 2021-08-10.
+//
+
+import Foundation
+
+struct UserDefaultKeys
+{
+    static let isUserLoggedInKey = "isUserLoggedInKey"
+    
+    
+    
+}
