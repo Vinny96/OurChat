b5106ac0cceb1c443363a4ac120d7a0dc42928d6
Vinojen Gengatharan <gengatharanvinojen@gmail.com>
2021-08-11 12:49:42 -0400

    Create ChatAppUser model that will be used for logging into the app and for database operations

--- OurChat/OurChat/Models/ChatAppUser.swift
+++ OurChat/OurChat/Models/ChatAppUser.swift
@@ -7,4 +7,23 @@
 
 import Foundation
 
-
+struct ChatAppUser
+{
+    // properties
+    private(set) internal var firstName : String
+    private(set) internal var lastName  : String
+    private(set) internal var email : String
+    internal var safeEmail : String {
+        var newEmailToReturn = email.replacingOccurrences(of: "@", with: "-")
+        newEmailToReturn = newEmailToReturn.replacingOccurrences(of: ".", with: "-")
+        return newEmailToReturn
+    }
+    
+    // initalizers
+    init(firstNameVal : String, lastNameVal : String, emailVal : String)
+    {
+        firstName = firstNameVal
+        lastName = lastNameVal
+        email = emailVal
+    }
+}
