713411c9193a31df092d855e0cf193efc4ad5900
Vinny96 <46853169+Vinny96@users.noreply.github.com>
2021-08-12 12:10:08 -0400

    Add files via upload
    An enum that contains Database errors that will be used.

---
+++ OurChat/OurChat/Resources/DatabaseManagerError.swift
@@ -0,0 +1,16 @@
+//
+//  DatabaseManagerError.swift
+//  OurChat
+//
+//  Created by Vinojen Gengatharan on 2021-08-11.
+//
+
+import Foundation
+
+enum DatabaseManagerError
+{
+    case failure
+    case success
+}
+
+
