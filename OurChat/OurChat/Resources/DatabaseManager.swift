be469a7cf1e608213eb1d0d5fcb5b86331ae09f8
Vinny96 <46853169+Vinny96@users.noreply.github.com>
2021-09-03 13:37:45 -0400

    Add New Functionality
    Add functionality that will add the new user to the users child node in the DB along with previous addition operations in the DB. If the users node does not exist the function will create the users child node and append add the new user to the users node.

--- OurChat/OurChat/Resources/DatabaseManager.swift
+++ OurChat/OurChat/Resources/DatabaseManager.swift
@@ -29,15 +29,62 @@ final class DatabaseManager
             "lastName" : lastName
         ]
         
+        // here we are writing our new user to the database and this is used solely for checking if the user exists
         databaseReference.child(safeEmail).setValue(valueToInsert) { error, _ in
             guard error == nil else
             {
                 completion(.failure(error!))
                 return
             }
-            // success
-            let successString = "Insertion was successful"
-            completion(.success(successString))
+        }
+        
+        // so now we need to see if the collection for users exists and if it does we append to that. If it does not we have to create it
+        databaseReference.child("users").observeSingleEvent(of: .value) {[weak self] dataSnapShot in
+            // so we need to check to see if a value exists here and if it does we can just create a new collection and append it. If not we will have to create the collection.
+            guard let strongSelf = self else{return}
+
+            if var usersCollection = dataSnapShot.value as? [[String : String]]
+            {
+                // so here the users collection exists
+                let arrayToAppend : [String : String] = [
+                    "safe_email" : chatAppUserToInsert.safeEmail,
+                    "full_name" : chatAppUserToInsert.firstName + " " + chatAppUserToInsert.lastName
+                ]
+                
+                usersCollection.append(arrayToAppend)
+                strongSelf.databaseReference.child("users").setValue(usersCollection, withCompletionBlock: { error, _ in
+                    guard error == nil else
+                    {
+                        print("There was an error in writing the newUser to the child Users in the database")
+                        completion(.failure(error!))
+                        return
+                    }
+                    // success
+                    let successString = "Successfully wrote the newUser to the database and to the users child node in the database"
+                    completion(.success(successString))
+                })
+                
+            }
+            else
+            {
+                // so here the users collection does not exist
+                var newUsersCollection : [[String : String]] = [[String : String]]()
+                let nestedArrayToAppend : [String : String] = [
+                    "safe_email" :  chatAppUserToInsert.safeEmail,
+                    "full_name" : chatAppUserToInsert.firstName + " " + chatAppUserToInsert.lastName
+                ]
+                newUsersCollection.append(nestedArrayToAppend)
+                strongSelf.databaseReference.child("users").setValue(newUsersCollection) { error, _ in
+                    guard error == nil else {
+                        print("There was an error in writing the newUsersCollection to the users child node in the database")
+                        completion(.failure(error!))
+                        return
+                    }
+                    // success
+                    let successString = "Successfully created a newUsersCollection, appended the first users to this collection and wrote the newUsersCollection to the database"
+                    completion(.success(successString))
+                }
+            }
         }
     }
     
@@ -57,5 +104,18 @@ final class DatabaseManager
         
     }
     
+    /// gets all of the users from the database
+    public func getAllUsersFromDB(completion : @escaping (Result<[[String : String]], Error>) -> Void)
+    {
+        databaseReference.child("users").observeSingleEvent(of: .value) { dataSnapShot in
+            guard let usersCollection = dataSnapShot.value as? [[String : String]] else {
+                print("Not able to fetch the usersCollection from the users child node in the DB")
+                completion(.failure(DatabaseManagerError.failedToFetch))
+                return
+            }
+            // success
+            completion(.success(usersCollection))
+        }
+    }
     
 }
