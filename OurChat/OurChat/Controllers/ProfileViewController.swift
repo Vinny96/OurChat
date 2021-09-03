9e1629cded36721d884ddc3f69f8c0230842cb34
Vinny96 <46853169+Vinny96@users.noreply.github.com>
2021-09-03 13:35:59 -0400

    UI Additions
    Add tableHeaderView that will house the searchbar.

--- OurChat/OurChat/Controllers/ProfileViewController.swift
+++ OurChat/OurChat/Controllers/ProfileViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 import FirebaseAuth
 import FBSDKLoginKit
 import GoogleSignIn
+import JGProgressHUD
 
 class ProfileViewController: UIViewController, UITableViewDataSource {
 
@@ -17,20 +18,101 @@ class ProfileViewController: UIViewController, UITableViewDataSource {
     
     
     // properties
+    let spinner = JGProgressHUD(style: .dark)
+    let storageManagerReference = StorageManager.shared
     let tableViewData = ["Log Out"]
+    let standardUserDefaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         intitalizeVC()
     }
         
+    // MARK: - UI Functions
+    private func createTableHeader() -> UIView?
+    {
+        guard let loggedInUserEmail = standardUserDefaults.value(forKey: UserDefaultKeys.loggedInUserSafeEmail) as? String else{return nil}
+        
+        let fileName = loggedInUserEmail + "_profilePicture.jpg"
+        let path = "images/"+fileName
+
+        let tableHeaderView = UIView(frame: CGRect(x: 0, y: 0, width: self.view.width, height: 300))
+        tableHeaderView.backgroundColor = .link
+        let imageView = UIImageView(frame: CGRect(x: (tableHeaderView.width - 150) / 2, y: 75, width: 150, height: 150))
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = .white
+        imageView.layer.borderColor = UIColor.white.cgColor
+        imageView.layer.borderWidth = 3
+        imageView.layer.masksToBounds = true
+        imageView.layer.cornerRadius = imageView.width / 2
+        tableHeaderView.addSubview(imageView)
+        setImageForProfileImageView(pathProvided: path, imageViewToUse: imageView)
+        return tableHeaderView
+    }
+    
+    private func setImageForProfileImageView(pathProvided : String, imageViewToUse : UIImageView)
+    {
+        spinner.show(in: imageViewToUse)
+        // so we want to see if the downloadURL exists in the userdefaults and if it does we will use that one. If it does not we will have to use the storageManager to get it
+        if let downloadURL = standardUserDefaults.value(forKey: UserDefaultKeys.userProfilePictureDownloadURLKey) as? URL
+        {
+            URLSession.shared.dataTask(with: downloadURL) { data, _, error in
+                guard let safeData = data, error == nil else {
+                    print("There was an error in downloading the image from the URL provided. \(error!)")
+                    return
+                }
+                // success
+                DispatchQueue.main.async {
+                    self.spinner.dismiss()
+                    imageViewToUse.image = UIImage(data: safeData)
+                }
+            }.resume()
+        }
+        else
+        {
+            // so here we were not able to get a URL from the value saved into the persistent store
+            var downloadURLAsString : String?
+            storageManagerReference.getDownloadURL(for: pathProvided) {[weak self] result in
+                switch result
+                {
+                case .success(let downloadURL):
+                    downloadURLAsString = downloadURL
+                    print("Success in getting the downloadURL from the profile view controller: \(downloadURL)")
+                    guard let safeDownloadURLAsString = downloadURLAsString else{return}
+                    self?.standardUserDefaults.set(safeDownloadURLAsString, forKey: UserDefaultKeys.userProfilePictureDownloadURLKey)
+                    guard let downloadURLToUse = URL(string: safeDownloadURLAsString) else {return}
+                    
+                    URLSession.shared.dataTask(with: downloadURLToUse) { data, _, error in
+                        guard let safeData = data, error == nil else {
+                            print("There was an error in getting data from the downloadURL provided. \(error!)")
+                            return
+                        }
+                        // success
+                        DispatchQueue.main.async {
+                            print("spinner should be dismissed by now")
+                            self?.spinner.dismiss()
+                            imageViewToUse.image = UIImage(data: safeData)
+                        }
+                    }.resume()
+                    
+                case .failure(let error):
+                    print("There was an error in getting the downloadURL from the path provided. \(error)")
+                    return
+                }
+            }
+        }
+    }
+    
+    
     // MARK: - Functions
     private func intitalizeVC()
     {
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.tableHeaderView = createTableHeader()
     }
+    
     
     
 }
@@ -79,10 +161,12 @@ extension ProfileViewController: UITableViewDelegate
         do
         {
             try FirebaseAuth.Auth.auth().signOut()
+            // here is where we want to clear the userDefaults curently logged in user email.
+            standardUserDefaults.set("placeholder", forKey: UserDefaultKeys.userProfilePictureDownloadURLKey)
+            standardUserDefaults.set("emailPlaceHolder", forKey: UserDefaultKeys.loggedInUserSafeEmail)
             let vc = LoginViewController()
             let nav = UINavigationController(rootViewController: vc)
             nav.modalPresentationStyle = .fullScreen
-            // here is where we want to clear the userDefaults curently logged in user email.
             present(nav, animated: true)
         }
         catch
