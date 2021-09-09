20df605c79fc18ebfd0235d5abdc4d8cb91561c4
Vinny96 <46853169+Vinny96@users.noreply.github.com>
2021-09-09 18:31:51 -0400

    Extension Add
    Add extension to MessageModel that will return a string for each MessageKind.

--- OurChat/OurChat/Models/Message.swift
+++ OurChat/OurChat/Models/Message.swift
@@ -14,7 +14,38 @@ struct Message : MessageType
     var messageId: String
     var sentDate: Date
     var kind: MessageKind
-    
+}
+
+extension MessageKind
+{
+    var messageKindString : String
+    {
+        switch self
+        {
+        case .text(_):
+            return "text"
+        case .attributedText(_):
+            return "attributedText"
+        case .photo(_):
+            return "photo"
+        case .video(_):
+            return "video"
+        case .location(_):
+            return "location"
+        case .emoji(_):
+            return "emoji"
+        case .audio(_):
+            return "audio"
+        case .contact(_):
+            return "contact"
+        case .linkPreview(_):
+            return "linkPreview"
+        case .custom(_):
+            return "custom"
+        }
+        
+        
+    }
     
     
 }
