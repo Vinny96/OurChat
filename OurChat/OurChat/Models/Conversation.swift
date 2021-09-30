be882242b28bf1543ac3838ddc52ee0063356946
Vinny96 <46853169+Vinny96@users.noreply.github.com>
2021-09-30 16:49:21 -0400

    Create Conversation Model
    This conversation model is used to display the conversations we have in the ConversationViewController

---
+++ OurChat/OurChat/Models/Conversation.swift
@@ -0,0 +1,16 @@
+//
+//  Conversation.swift
+//  OurChat
+//
+//  Created by Vinojen Gengatharan on 2021-09-20.
+//
+
+import Foundation
+
+struct Conversation
+{
+    let otherUserName : String
+    let conversationID : String
+    let latestMessage : LatestMessage
+    let otherUserEmail : String
+}
