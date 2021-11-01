63b04169bba68d5c87ffed8153681b0bc4d95bf4
Vinny96 <46853169+Vinny96@users.noreply.github.com>
2021-11-01 11:06:17 -0400

    Add new functionality
    Add new functionality that will put users photo into a conversations folder in Firebase Storage

--- OurChat/OurChat/Resources/Storage/StorageManager.swift
+++ OurChat/OurChat/Resources/Storage/StorageManager.swift
@@ -17,8 +17,8 @@ final class StorageManager
     // functions
     
     /// Only UploadsProfilePictureToCloud and if successful we should get back the downloadURLAsAString
-    public typealias uploadProfilePictureCompletion = (Result<String,Error>) -> Void
-    public func uploadProfilePictureToStorage(dataToUpload : Data, fileName : String, completion : @escaping uploadProfilePictureCompletion)
+    internal typealias uploadProfilePictureCompletion = (Result<String,Error>) -> Void
+    internal func uploadProfilePictureToStorage(dataToUpload : Data, fileName : String, completion : @escaping uploadProfilePictureCompletion)
     {
         let referenceToChildURL = storageReference.child("images/"+fileName)
         
@@ -40,6 +40,54 @@ final class StorageManager
             }
         }
     }
+    
+    /// gets the download URL from the path provided
+    internal func getDownloadURL(for path : String, completionHandler : @escaping (Result<String,Error>) -> Void)
+    {
+        let reference = storageReference.child(path)
+        reference.downloadURL { url, error in
+            guard let safeURl = url, error == nil else{
+                print("There was an error in getting the downloadURL fro the path.")
+                completionHandler(.failure(StorageError.downloadURLError))
+                return
+            }
+            // here it was successful so we want to pass the downloadURL as a string into the closure
+            completionHandler(.success(safeURl.absoluteString))
+        }
+    }
+    
+    
+    /// uploads the image to the conversations subfolder whose id matches the conversation id provided
+    internal typealias uploadImageToConversationCompletion = (Result<String,Error>) -> Void
+    internal func uploadImageToConversationStorage(data : Data, fileName : String, conversationID : String ,completion : @escaping uploadImageToConversationCompletion)
+    {
+        // so first thing is first and we want to define the path
+        //let path = "conversations/\(conversationID)" + fileName
+        let path = ("conversations/\(conversationID)/\(fileName)")
+        // now that we have our path we want to put data at this location
+        storageReference.child(path).putData(data, metadata: nil) {[weak self] _, error in
+            guard let strongSelf = self else {return}
+            guard error == nil else {
+                completion(.failure(StorageError.uploadFileError))
+                return
+            }
+            // success so now we want to get the download URL back
+            print("Successfully put our data at the specified location")
+            strongSelf.getDownloadURL(for: path) { result in
+                switch result
+                {
+                case .success(let downloadURlAsString):
+                    completion(.success(downloadURlAsString))
+                
+                case .failure(let downloadURLError):
+                    completion(.failure(downloadURLError))
+                }
+            }
+        }
+    }
+
+    
+    
     
     
     
