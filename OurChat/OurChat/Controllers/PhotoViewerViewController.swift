ded7eab84e2d725621482f0c66f2f0c1f5add252
Vinny96 <46853169+Vinny96@users.noreply.github.com>
2021-11-02 14:18:30 -0400

    Create UI
    Create UI for the photo picker view controller

--- OurChat/OurChat/Controllers/PhotoViewerViewController.swift
+++ OurChat/OurChat/Controllers/PhotoViewerViewController.swift
@@ -6,24 +6,54 @@
 //
 
 import UIKit
+import SDWebImage
 
 class PhotoViewerViewController: UIViewController {
 
+    //MARK: -Properties
+    private let imageView : UIImageView = {
+        let imageViewToReturn = UIImageView()
+        imageViewToReturn.contentMode = .scaleAspectFill
+        imageViewToReturn.clipsToBounds = true
+        return imageViewToReturn
+    }()
+    
+    private var url : URL
+    
+    
+    //MARK: - Initializers and System Called Functions
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .black
+        initializeImageView()
+        displayImage()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        view.addSubviews(views: imageView)
     }
-    */
-
+    
+    init(urlToUse : URL)
+    {
+        url = urlToUse
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - UI Functions
+    private func initializeImageView()
+    {
+        imageView.frame = view.bounds
+    }
+    
+    private func displayImage()
+    {
+        print("Image should be displayed now")
+        imageView.sd_setImage(with: url, completed: nil)
+    }
+    
 }
