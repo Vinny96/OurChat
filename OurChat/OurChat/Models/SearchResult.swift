217a987352199b68aaba6510059395467f6e0efe
Vinny96 <46853169+Vinny96@users.noreply.github.com>
2022-01-13 21:11:51 -0500

    Create SearchResult Model
    Create SearchResult model that is used to hold the data retrieved from the search for user functionality in the new conversation view controller

---
+++ OurChat/OurChat/Models/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  OurChat
+//
+//  Created by Vinojen Gengatharan on 2022-01-13.
+//
+
+import Foundation
+
+struct SearchResult
+{
+    internal var fullName : String
+    internal var email : String
+}
